67cdb4cff2dd266f330149c7f5a6868161ca5097
thiagosouunilavras <thiagosadm@souunilavras.com>
2025-04-13 14:44:38 -0300

    hello

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
-//echo "Hello Azure";
-echo "A inteligência artificial (IA) está transformando o mundo ao trazer inovações para diversas áreas, como saúde, tecnologia, educação e muito mais.";
+echo "Hello Azure";
+
