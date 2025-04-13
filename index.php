aa532e9dee5ec051c8e051905a5aa75aaa6397d0
thiagosouunilavras <thiagosadm@souunilavras.com>
2025-04-13 14:43:37 -0300

    textiIA

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
 
 //echo "Hello Azure";
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <form method="POST" action="processa_login.php">
-        <label for="usuario">Usuário:</label>
-        <input type="text" id="usuario" name="usuario" required><br><br>
-
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" name="senha" required><br><br>
-
-        <button type="submit">Logar</button>
-    </form>
-</body>
-</html>
+echo "A inteligência artificial (IA) está transformando o mundo ao trazer inovações para diversas áreas, como saúde, tecnologia, educação e muito mais.";
