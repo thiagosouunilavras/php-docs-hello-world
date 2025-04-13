7dd3885d23e6ca285427bfab4bd5005c7765a02b
thiagosouunilavras <thiagosadm@souunilavras.com>
2025-04-13 14:38:10 -0300

    html

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "Hello Azure";
+//echo "Hello Azure";
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="POST" action="processa_login.php">
+        <label for="usuario">Usuário:</label>
+        <input type="text" id="usuario" name="usuario" required><br><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required><br><br>
+
+        <button type="submit">Logar</button>
+    </form>
+</body>
+</html>
